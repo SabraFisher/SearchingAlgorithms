8cac0918ff64d97320808bfd8bf92b3a07d075c5
Sabra Fisher <labcoatkitty@gmail.com>
2025-10-07 13:21:04 -0700

    proper naming on function in search algo file -slf

--- SearchingAlgorithms/SearchAlgorithms.cs
+++ SearchingAlgorithms/SearchAlgorithms.cs
@@ -25,7 +25,7 @@ namespace SearchingAlgorithms
         //    return -1; // Target not found
         //}
 
-        public static int BinarySearch<T>(T[] arr, int low, int high, T searchTerm) where T : IComparable<T> 
+        public static int RecursiveBinarySearch<T>(T[] arr, int low, int high, T searchTerm) where T : IComparable<T> 
         {
             int mid = (low + high) / 2;
             if (arr[mid].Equals(searchTerm))
@@ -35,7 +35,7 @@ namespace SearchingAlgorithms
 
             if (arr[mid].CompareTo(searchTerm) > 0 && low < high)
             {
-                return BinarySearch(arr, low, mid - 1, searchTerm);
+                return RecursiveBinarySearch(arr, low, mid - 1, searchTerm);
             }
 
             else if (low >= high)
@@ -44,14 +44,32 @@ namespace SearchingAlgorithms
             }
             else if (arr[mid].CompareTo(searchTerm) < 0)
             {
-                return BinarySearch(arr, mid + 1, high, searchTerm);
+                return RecursiveBinarySearch(arr, mid + 1, high, searchTerm);
             }
             else
             {
-                return BinarySearch(arr, low, mid - 1, searchTerm);
+                return RecursiveBinarySearch(arr, low, mid - 1, searchTerm);
             }
         }
 
+        static int IterativeBinarySearch(int[] arr, int x)
+        {
+            int low = 0, high = arr.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                //check if x id pressent at mid
+                if (arr[mid] == x)
+                    return mid;
+                //if x is greater, ignore left half
+                if (arr[mid] < x)
+                    low = mid + 1;
+                //if x is smaller, ignore right half
+                else
+                    high = mid - 1;
+            }
+            return -1;
+        }   
 
 
          public static int LinearSearch(int[] array, int target)

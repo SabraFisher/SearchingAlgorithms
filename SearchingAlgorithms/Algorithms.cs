581efc2bb1b4d41c8f99ee6de6dddc7ed1de1f61
Sabra Fisher <labcoatkitty@gmail.com>
2025-10-14 20:37:04 -0700

    binary recursive search

--- SearchingAlgorithms/Algorithms.cs
+++ SearchingAlgorithms/Algorithms.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace SearchingAlgorithms
 {
     static class Algorithms
-            {
-        //todo- refactor linear search to be generic
+        {
+        //Searching algorithms
+        //refactored linear search to be generic
         public static int LinearSearch<T>(T[] arr, T searchTerm) where T : IComparable<T>  //returns first instance of searchTerm
         {
             for (int i = 0; i < arr.Length; i++) //search from the beginning of the array to the end
@@ -18,77 +19,39 @@ namespace SearchingAlgorithms
                      return i ;
                 }
             }
-            return -1; // Target not found
+
+            return -1; // if target not found
         }
 
-        public static int LinearSearch(int[] array, int searchTerm)  //returns first instance of searchTerm
-        {
-            for (int i = 0; i < array.Length; i++) //search from the beginning of the array to the end
+        //data must be sorted prior to searching
+        //function: Takes a sorted array, splits it in two and searches both halves recursively for a matching search term
+        public static int BinarySearch<T>(T[] arr, int low, int high, T searchTerm) where T : IComparable<T>
+        {   // only works on sorted arrays
+            //base case - if low is greater than high, the searchTerm is not in the array
+            int mid = low + ((high-low) / 2);
+
+            if (arr[mid].Equals(searchTerm))
             {
-                if (array[i] == searchTerm)
-                {  //is this the search term?
-                    return i;
-                }
+                return mid;
             }
-            return -1; // Target not found
-        }
-        //public static int BinarySearch(int[] array, int target)
-        //{
-        //    int left = 0;
-        //    int right = array.Length - 1;
-        //    while (left <= right)
-        //    {
-        //        int mid = left + (right - left) / 2;
-        //        if (array[mid] == target)
-        //            return mid;
-        //        if (array[mid] < target)
-        //            left = mid + 1;
-        //        else
-        //            right = mid - 1;
-        //    }
-        //    return -1; // Target not found
-        //}
 
-        public static int RecursiveBinarySearch<T>(T[] arr, int low, int high, T searchTerm) where T : IComparable<T>
-        {
-            int mid = (low + high) / 2;
-
-            if (high <= low)
+            //recursive case
+            if (arr[mid].CompareTo(searchTerm) < 0) // mid is less than searchTerm
             {
-
-                if (arr[mid].Equals(searchTerm))
-                {
-                    return mid;
-                }
-
-                if (arr[mid].CompareTo(searchTerm) > 0)
-                {
-                    return RecursiveBinarySearch(arr, low, mid - 1, searchTerm);
-                }
-
-                return RecursiveBinarySearch(arr, mid + 1, high, searchTerm);
+                BinarySearch(arr, mid + 1, high, searchTerm);
             }
-            return -1;
+            
+            else if (arr[mid].CompareTo(searchTerm) > 0) // mid is greater than searchTerm
+            {
+                BinarySearch(arr, low, mid - 1, searchTerm);
+            }
+
+            return -1; // if target not found
         }
 
-        static int IterativeBinarySearch(int[] arr, int x)
-        {
-            int low = 0, high = arr.Length - 1;
-            while (low <= high)
-            {
-                int mid = low + (high - low) / 2;
-                //check if x id pressent at mid
-                if (arr[mid] == x)
-                    return mid;
-                //if x is greater, ignore left half
-                if (arr[mid] < x)
-                    low = mid + 1;
-                //if x is smaller, ignore right half
-                else
-                    high = mid - 1;
-            }
-            return -1;
-        }   
+        
+
+       
 
 
       
